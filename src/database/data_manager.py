491aa2ccb0e77662fbc23bbf634d5a0a97c39d32
BuildTools <unconfigured@null.spigotmc.org>
2019-10-24 21:14:43 +0100

    csv to database function

--- src/database/data_manager.py
+++ src/database/data_manager.py
@@ -18,7 +18,7 @@ def read_csv_to_dictionary(filename):
         for row in csv_reader:
             # gets the names of the columns in the csv file.
             if line_count == 0:
-                print(f"The column names are {', '.join(row)}.")
+                # print(f"The column names are {', '.join(row)}.")
                 line_count += 1
             else:
                 row_data = {"id": row[0],
@@ -52,20 +52,34 @@ def database_to_csv(database_filename, csv_filename):
 
 
 def csv_to_database(csv_filename, database_filename):
-    pass
+    try:
+        with open(csv_filename, mode='r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            line_count = 0
+
+            connection = sqlite3.connect(database_filename)
+            with connection:
+                
+                for row in csv_reader:
+
+                    if line_count == 0:
+                        line_count += 1
+                        continue
+                    else:
+                        print(row)
+                        connection.execute("INSERT INTO questions(id, question, answer, wrong_answer_1, wrong_answer_2, wrong_answer_3, difficulty) values (?, ?, ?, ?, ?, ?, ?)",
+                                           (None, row[1], row[2], row[3], row[4], row[5], row[6]))
+
+    except:
+        raise
 
 def create_database(database_filename):
     connection = sqlite3.connect(database_filename)
     c = connection.cursor()
-    c.execute('''CREATE TABLE "questions" ( "id" INTEGER UNIQUE PRIMARY KEY, "question" TEXT, "answer" TEXT, "wrong_answer_1" TEXT, "wrong_answer_2" TEXT, "wrong_answer_3" TEXT, "difficulty" INTEGER )''')
+    c.execute('''CREATE TABLE "questions" ( "id" INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT, "question" TEXT, "answer" TEXT, "wrong_answer_1" TEXT, "wrong_answer_2" TEXT, "wrong_answer_3" TEXT, "difficulty" INTEGER )''')
     connection.commit()
     connection.close()
 
-if __name__ == "__main__":
-    d = read_csv_to_dictionary('questions.csv')
-    for row in d:
-        print(row)
-    
 
-    create_database("questions.db")
-    #database_to_csv("questions.csv", "testing_questions.csv")
+if __name__ == "__main__":
+    pass
