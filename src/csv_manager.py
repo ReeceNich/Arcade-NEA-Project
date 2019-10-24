f5ed984ff958c090fe1dc34785dd479731ff4ac5
BuildTools <unconfigured@null.spigotmc.org>
2019-10-24 19:19:47 +0100

    refactored read csv function

--- src/csv_manager.py
+++ src/csv_manager.py
@@ -1,39 +1,38 @@
 """
-Opens the CSV file of all the questions and returns a list with items as dictionaries containing id, question, answers and difficulty.
+Opens the CSV file of all the questions and returns a list with items as dictionaries containing id, question, answer, wrong answers (1 to 3) and difficulty.
 """
 import csv
 
-def read_csv(filename):
+def read_csv_to_dictionary(filename):
     # open the csv file
     with open(filename, mode='r') as csv_file:
         csv_reader = csv.reader(csv_file)
         line_count = 0
-        data = []
+        all_data = []
 
+        # for each row, get the columns and append to a dictionary.
         for row in csv_reader:
+            # gets the names of the columns in the csv file.
             if line_count == 0:
                 print(f"The column names are {', '.join(row)}.")
-                columns = row
                 line_count += 1
-
             else:
-                #print(f"ID: {row['id']}, Q: {row['question']}, A1: {row['correct_answer']}, A2: {row['wrong_answer_1']}, A3: {row['wrong_answer_2']}, A4: {row['wrong_answer_3']}, D: {row['difficulty']}/10.")
-                temp_dict = {columns[0]: row[0],
-                            columns[1]: row[1],
-                            columns[2]: row[2],
-                            columns[3]: row[3],
-                            columns[4]: row[4],
-                            columns[5]: row[5],
-                            columns[6]: row[6]}
-                data.append(temp_dict)
+                row_data = {"id": row[0],
+                            "questions": row[1],
+                            "correct_answer": row[2],
+                            "wrong_answer_1": row[3],
+                            "wrong_answer_2": row[4],
+                            "wrong_answer_3": row[5],
+                            "difficulty": row[6]}
+                all_data.append(row_data)
+                # print(f"ID: {row['id']}, Q: {row['question']}, A1: {row['correct_answer']}, A2: {row['wrong_answer_1']}, A3: {row['wrong_answer_2']}, A4: {row['wrong_answer_3']}, D: {row['difficulty']}/5.")
                 line_count += 1
-
-        print(f"Total lines = {line_count}.")
-        return data
+        
+        return all_data
 
 
 if __name__ == "__main__":
-    d = read_csv('questions.txt')
+    d = read_csv_to_dictionary('questions.txt')
     for row in d:
         print(row)
     
