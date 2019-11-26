776150c69bbe09fbd082bb66cddcf0cc3182cb3c
BuildTools <magicrwn@gmail.com>
2019-11-26 20:40:53 +0000

    question class

--- src/database_manager.py
+++ src/database_manager.py
@@ -21,17 +21,29 @@ class DB:
         self.conn.commit()
 
 
+class Question:
+    def __init__(self, question, answer, wrong_1, wrong_2, wrong_3, subject, difficulty):
+        self.question = question
+        self.answer = answer
+        self.wrong_1 = wrong_1
+        self.wrong_2 = wrong_2
+        self.wrong_3 = wrong_3
+        self.subject = subject
+        self.difficulty = difficulty
+
+
 def example(conn):
     db = DB(conn)
 
     db.setup()
 
-    db.insert(Track("Happy Birthday", 10))
-    db.insert(Track("auld lang syne", 12))
-    db.insert(Track("Default Dance", 42069))
-    tracks = db.query_all_tracks()
-    for track in tracks:
-        print(f"{track.title} played has been played {track.plays} times")
+    #db.insert(Track("Happy Birthday", 10))
+    
+    #tracks = db.query_all_tracks()
+    #for track in tracks:
+    #    print(f"{track.title} played has been played {track.plays} times")
+
+
 def postgres_example():
     conn = psycopg2.connect("dbname='database1' user=postgres password='password' host='localhost' port='5432'")
     example(conn)
