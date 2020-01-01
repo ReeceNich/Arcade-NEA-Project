bad322f8f2e0c1476b501a328cf81ce5f12df885
BuildTools <magicrwn@gmail.com>
2020-01-01 22:41:57 +0000

    basic error handling

--- src/run_falling_man.py
+++ src/run_falling_man.py
@@ -3,12 +3,16 @@ from login_window import Login
 login = Login()
 login.draw_window()
 
-from falling_man import *
+if login.username and login.passcode:
+    from falling_man import *
 
-window = MyGame(width, height, title, login.username, login.passcode)
-window.setup()
+    window = MyGame(width, height, title, login.username, login.passcode)
+    window.setup()
 
-print(login.username)
-print(login.passcode)
+    print(login.username)
+    print(login.passcode)
 
-arcade.run()
+    arcade.run()
+
+else:
+    print("didnt enter details")
