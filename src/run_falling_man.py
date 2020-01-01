9fcf34d2ff846514692a3f7f35cd899319907cf7
BuildTools <magicrwn@gmail.com>
2020-01-01 23:05:54 +0000

    rename username to email

--- src/run_falling_man.py
+++ src/run_falling_man.py
@@ -3,13 +3,13 @@ from login_window import Login
 login = Login()
 login.draw_window()
 
-if login.username and login.passcode:
+if login.email and login.passcode:
     from falling_man import *
 
-    window = MyGame(width, height, title, login.username, login.passcode)
+    window = MyGame(width, height, title, login.email, login.passcode)
     window.setup()
 
-    print(login.username)
+    print(login.email)
     print(login.passcode)
 
     arcade.run()
