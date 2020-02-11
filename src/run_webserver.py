d4e03de27d66d246073640c8d4e509f815a4abe7
BuildTools <magicrwn@gmail.com>
2020-02-11 14:34:07 +0000

    now passes database into webserver file

---
+++ src/run_webserver.py
@@ -0,0 +1,8 @@
+import psycopg2
+from database.database_manager import DatabaseManager
+from website.webserver import MyHandler, run
+
+db = DatabaseManager(psycopg2.connect("dbname='database1' user=postgres password='pass' host='localhost' port='5432'"))
+
+if __name__ == "__main__":
+    run(db)
